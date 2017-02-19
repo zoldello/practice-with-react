1637f48eb919516cd5589886f1f81104f08dc234
Phil <zoldello@gmail.com>
2017-02-19 15:53:14 -0600

    added basic redux

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+import {bindActionCreators} from 'redux';
 
-export default class CoursePage extends React.Component {
+class CoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title:"" }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState( { course: course } );
+  }
+
+  onClickSave() {
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render () {
     return (
       <div>
        <h1>Courses</h1>
+       {this.props.courses.map(this.courseRow)}
+       <h2>Add Courses</h2>
+       <input
+         type="text"
+         onChange={this.onTitleChange}
+         value={this.state.course.title}
+        />
+
+       <input
+         type="submit"
+         value="Save"
+         onClick={this.onClickSave}
+        />
       </div>
     );
   }
 }
+
+CoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursePage);
